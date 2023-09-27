f3b083a8f38795e30ed4928ff4c13617992f11da
Deepan-Ragavan <deebanyathiraja2002@gmail.com>
2023-09-27 17:42:47 +0530

    Specificity Added : Toggle

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,30 +1,44 @@
-let darkMode=()=>{
-    document.body.style.backgroundColor = '#202124'
-    document.body.style.color = '#fff'
+let darkMode = () => {
+    document.body.classList.toggle('dark-mode')
 
-    let darkBtn = document.getElementById('darkBtn')
-    darkBtn.setAttribute('class', 'btn btn-light')
-    darkBtn.textContent = 'Light Mode'
+    if (document.body.classList.contains('dark-mode')) {
+        let darkBtn = document.getElementById('darkBtn')
+        darkBtn.setAttribute('class', 'btn btn-dark')
+        darkBtn.innerHTML = '<i class="fa-regular fa-sun fa-2xl"></i>'
 
-    let icon = document.getElementById('icon')
-    icon.style.display = 'none'
+        let icon = document.getElementById('icon')
+        icon.style.display = 'none'
 
-    let signUpBtn = document.getElementById('signInBtn')
-    signUpBtn.setAttribute('class', 'btn btn-secondary')
+        let signUpBtn = document.getElementById('signInBtn')
+        signUpBtn.setAttribute('class', 'btn btn-outline-primary')
 
-    let googleImg = document.getElementById('googleImg')
-    googleImg.setAttribute('src','https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_272x92dp.png')
+        let googleImg = document.getElementById('googleImg')
+        googleImg.setAttribute('src', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_272x92dp.png')
 
-    let searchBtn = document.querySelector('.row1>button')
-    searchBtn.setAttribute('class','btn btn-outline-light')
+        let searchBtn = document.querySelector('.row1>button')
+        searchBtn.setAttribute('class', 'btn btn-outline-light')
 
-    let searchBtn2 = document.getElementById('btn2')
-    searchBtn2.setAttribute('class', 'btn btn-outline-light')
+        let searchBtn2 = document.getElementById('btn2')
+        searchBtn2.setAttribute('class', 'btn btn-outline-light')
+    }
+    else{
+        let darkBtn = document.getElementById('darkBtn')
+        darkBtn.setAttribute('class', 'btn btn-light')
+        darkBtn.innerHTML = '<i class="fa-regular fa-moon fa-2xl"></i>'
 
-    let googleTranslate = document.getElementById('gTranslate')
-    googleTranslate.style.color = '#bdc1c6'
+        let icon = document.getElementById('icon')
+        icon.style.display = 'inline'
 
-    let footerEl = document.querySelector('footer')
-    footerEl.style.backgroundColor = '#171717'
-    footerEl.style.color = '#9aa0a6'
+        let signUpBtn = document.getElementById('signInBtn')
+        signUpBtn.setAttribute('class', 'btn btn-primary')
+
+        let googleImg = document.getElementById('googleImg')
+        googleImg.setAttribute('src', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
+
+        let searchBtn = document.querySelector('.row1>button')
+        searchBtn.setAttribute('class', 'btn')
+
+        let searchBtn2 = document.getElementById('btn2')
+        searchBtn2.setAttribute('class', 'btn')
+    }
 }
